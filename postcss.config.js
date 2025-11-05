a8bd02b24a6197e3e50281a01d1e4618c7a2266f
malikfarhanchk <malik.farhanchk@gmail.com>
2025-11-05 00:18:34 +0000

    Fix PostCSS configuration - add proper ES module syntax with plugins export

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// PostCSS configuration for Next.js
+// This file uses ES module syntax (required because package.json has "type": "module")
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
